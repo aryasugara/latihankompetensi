e0fda72ed18aca0b27fc0d10432d5f219d6606c1
aryasugara <sugara232425@gmail.com>
2023-05-02 12:57:42 +0700

    uji kompetensi

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+@extends('template.main')
+@section('konten')
+<div class="row">
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 mb-4">
+      <div class="card card-statistics">
+        <div class="card-body">
+          <div class="clearfix">
+            <div class="float-left">
+              <h4 class="text-danger">
+                <i class="fa fa-bar-chart-o highlight-icon" aria-hidden="true"></i>
+              </h4>
+            </div>
+            <div class="float-right">
+              <p class="card-text text-dark">Visitors</p>
+              <h4 class="bold-text">65,650</h4>
+            </div>
+          </div>
+          <p class="text-muted">
+            <i class="fa fa-exclamation-circle mr-1" aria-hidden="true"></i> 65% lower growth
+          </p>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
